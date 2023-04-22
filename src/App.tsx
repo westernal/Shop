31343e3e87ab0a5119264d59069382db43a85039
ali navidi <ali_navidi24@yahoo.com>
2023-04-22 20:04:52 +0330

    Header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CardList from "./components/Card/CardList";
+import Header from "./components/Header/header";
 
 function App() {
   const players: string[] = ["dalpeh", "westernal"];
   return (
     <main>
+      <Header />
       <CardList players={players} />
     </main>
   );
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="text-center p-5">
+      <h1 className="text-violet-800 text-3xl">Chess Stats</h1>
+    </header>
+  );
+};
+
+export default Header;
